8acff7930ffd6885483c334e047623e6eee1085d
plasma-27 <115122859+plasma-27@users.noreply.github.com>
2024-10-09 20:59:09 +0530

    Update projectRoutes.js

--- backend/routes/projectRoutes.js
+++ backend/routes/projectRoutes.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { createProject, getProjectDetails, updateProject, assignMentor } = require('../controllers/projectController');
 
-router.post('/projects', createProject); // Mentee creates a project
-router.get('/projects/:id', getProjectDetails); // Get project details
-router.put('/projects/:id', updateProject); // Update project status or details
-router.post('/projects/assign-mentor', assignMentor);
+router.post('/create-project', createProject); // Mentee creates a project
+router.get('/:id', getProjectDetails); // Get project details
+router.put('/:id', updateProject); // Update project status or details
+router.post('/assign-mentor', assignMentor);
 
 module.exports = router;
