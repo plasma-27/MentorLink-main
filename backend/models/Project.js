ea16a95eaa477732a6c74910fa6b352da8d39f5f
plasma-27 <115122859+plasma-27@users.noreply.github.com>
2024-10-09 20:58:16 +0530

    Update Project.js

--- backend/models/Project.js
+++ backend/models/Project.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const ProjectSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
+  github:{type:String},
   mentees: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], // Array of mentee IDs
   status: { type: String, enum: ['ongoing', 'completed'], default: 'ongoing' },
   mentors: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], // Array of mentor IDs
